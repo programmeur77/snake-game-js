07beab5877cc4a493056dec12352179fec61fe6e
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-09-29 10:22:43 +0200

    Implemented update function to make the snake moving

--- snake.js
+++ snake.js
@@ -2,13 +2,20 @@ export const SNAKE_SPEED = 2;
 
 const snakeBody = [{ x: 11, y: 11 }];
 
-export const update = () => {};
+export const update = () => {
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = [...snakeBody[i]];
+  }
+
+  snakeBody[0].x += 1;
+  snakeBody[0].y += 0;
+};
 
 export const draw = (gameBoard) => {
   snakeBody.forEach((segment) => {
     const snakeElement = document.createElement('div');
-    snakeElement.style.gridRowStart = segment.x;
-    snakeElement.style.gridColumnStart = segment.y;
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
     snakeElement.classList.add('snake');
     gameBoard.appendChild(snakeElement);
   });
