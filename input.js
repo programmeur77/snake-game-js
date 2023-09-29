fa07a228559aff66bfd201f444cf5723a1b44b93
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-09-29 10:28:49 +0200

    Implemented function to make snake moves

---
+++ input.js
@@ -0,0 +1,10 @@
+/**
+ * @file Manages the keyboard input to male the snake moves to desired direction
+ * @author Benoît Puech and Kyle from Web Dev Simplified
+ */
+
+let inputDirection = {x: 0, y: 0};
+
+export const getInputDirection = () => {
+    return inputDirection
+}
