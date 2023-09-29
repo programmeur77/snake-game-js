f94b02d966160ca1812ca052b41f9f0c0bdfb6cd
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-09-29 10:29:21 +0200

    COmmented some unused code and imported input function

--- snake.js
+++ snake.js
@@ -1,14 +1,17 @@
+import { getInputDirection } from './input';
+
 export const SNAKE_SPEED = 2;
 
 const snakeBody = [{ x: 11, y: 11 }];
 
 export const update = () => {
+  getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = [...snakeBody[i]];
   }
 
-  snakeBody[0].x += 1;
-  snakeBody[0].y += 0;
+  //   snakeBody[0].x += 0;
+  //   snakeBody[0].y += 1;
 };
 
 export const draw = (gameBoard) => {
