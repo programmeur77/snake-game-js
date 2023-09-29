13f171fdca69b6f082830f1c3d88bafe6b0aaf69
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2023-09-29 10:52:35 +0200

    Changed segment.x/y by food.x/y

--- food.js
+++ food.js
@@ -2,3 +2,15 @@
  * @file Manages Snake's food. Very similar to snake.js file
  * @author Benoît Puech and Kyle from Web Dev Simplified
  */
+
+let food = { x: 0, y: 0 };
+
+export const update = () => {};
+
+export const draw = (gameBoard) => {
+  const foodElement = document.createElement('div');
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
+  foodElement.classList.add('food');
+  gameBoard.appendChild(foodElement);
+};
